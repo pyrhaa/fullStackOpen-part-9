61964a6322df93cc588bbfe8fa9b6160c658a3ea
pyrhaa <sboubtane@protonmail.com>
2022-12-30 16:40:39 +0100

    9.5 in progress, need query param and ts module of bmicalcul

--- webBMI/index.ts
+++ webBMI/index.ts
@@ -5,6 +5,16 @@ app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
 });
 
+app.get('/bmi', (_req, res) => {
+  try {
+    return res.send('Hello Full Stack!');
+  } catch (error) {
+    return res.status(404).send({
+      error: 'malformatted parameters'
+    });
+  }
+});
+
 const PORT = 3003;
 
 app.listen(PORT, () => {
