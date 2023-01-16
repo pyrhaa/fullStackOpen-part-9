49954e52620b1a86abaa7c46f4bf700b353d1eb4
pyrhaa <sboubtane@protonmail.com>
2023-01-16 17:46:54 +0100

    error with var bmi

--- bodyMass/bmiCalculator.ts
+++ bodyMass/bmiCalculator.ts
@@ -47,3 +47,5 @@ function bmiResultat() {
 }
 
 bmiResultat();
+
+export { calculateBmi };
--- webBMI/index.ts
+++ webBMI/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { calculateBmi } from '../bodyMass/bmiCalculator';
 const app = express();
 
 app.get('/hello', (_req, res) => {
@@ -7,11 +8,24 @@ app.get('/hello', (_req, res) => {
 
 app.get('/bmi', (_req, res) => {
   try {
-    return res.send('Hello Full Stack!');
+    const { height, weight } = _req.query;
+    const validParameters: boolean =
+      !isNaN(Number(height)) && !isNaN(Number(weight));
+
+    if (!validParameters || !weight || !height) {
+      return res
+        .status(400)
+        .json({
+          error: 'malformatted parameters'
+        })
+        .end();
+    }
+    const bmi = calculateBmi(Number(height), Number(weight));
+    // console.log('bmi: ', bmi);
+
+    return null;
   } catch (error) {
-    return res.status(404).send({
-      error: 'malformatted parameters'
-    });
+    return res.status(404).send(error);
   }
 });
 
