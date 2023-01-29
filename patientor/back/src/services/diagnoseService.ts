e4807d68a100ccc92438ecf90f319b058a6f437e
pyrhaa <sboubtane@protonmail.com>
2023-01-29 14:43:58 +0100

    9.10 in progress

---
+++ patientor/back/src/services/diagnoseService.ts
@@ -0,0 +1,9 @@
+import diagnoseData from '../../data/diagnoses.json';
+
+const getDiagnoses = () => {
+  return diagnoseData;
+};
+
+export default {
+  getDiagnoses
+};
---
+++ patientor/back/src/services/patientService.ts
@@ -0,0 +1,9 @@
+import diagnoseData from '../../data/patients.json';
+
+const getPatients = () => {
+  return diagnoseData;
+};
+
+export default {
+  getPatients
+};
